4a69047136ca273bebcee4517c71278aa8aeb322
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-12-13 04:12:51 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
 class ApaliTaxi{
-    constructor(distance,carType){
+    constructor(distance,BasePriceUpto,BasePrice,ForNextkm,ForNextkmPrice,morethankm){
         this.distance = distance;
-        this.carType = carType;
+        this.BasePriceUpto = BasePriceUpto
+        this.BasePrice = BasePrice
+        this.ForNextkm = ForNextkm
+        this.ForNextkmPrice = ForNextkmPrice
+        this.morethankm = morethankm
     }
     cap = () =>{
-        // let capType = this.carType.toLowerCase()
-        minicap()
-        suvcap()
-        sedancap()
-    }
-
-    minicap = () =>{
-        if(this.distance < 75){
-            console.log(this.distance)
+        let result;
+        let max = this.distance-this.BasePriceUpto
+        if(this.distance <= this.BasePriceUpto){
+            result = this.BasePriceUpto;
+            console.log(result)
         }
+        else if(this.distance < this.ForNextkm){
+                result = ((max*this.ForNextkmPrice)+this.BasePrice)
+                console.log(result)
+        }
+        else if(this.distance <= 75 ){
+            result = (50+((max-(max-this.ForNextkmPrice))*this.ForNextkm)+((max-this.ForNextkmPrice)*8))
+            console.log(result)
+        }
+        else if(this.distance > this.morethankm){
+            result = this.distance*8
+            console.log(result)
+        }
+        // return result
     }
-
 }
 
-const name = new ApaliTaxi(10);
-name.cap()
+class Mini{
+    constructor(){
+    const name = new ApaliTaxi(75,BasePriceUpto,BasePrice,ForNextkm,ForNextkmPrice,morethan);
+    name.cap()
+    }
+}
+const name = new Mini; 
